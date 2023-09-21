4ad0a62051043a7674d82c5ed047d2924e001d83
Nicolas Agustin Martinez <59147377+neekoh15@users.noreply.github.com>
2023-09-21 20:43:48 -0300

    Create LrTF.py

---
+++ LrTF.py
@@ -0,0 +1,26 @@
+# Importar las bibliotecas necesarias
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generar datos de entrenamiento
+X_train = np.linspace(-1, 1, 100)
+y_train = 2 * X_train + np.random.randn(*X_train.shape) * 0.33
+
+# Crear el modelo
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(1, input_shape=(1,)))
+
+# Compilar el modelo
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# Entrenar el modelo
+model.fit(X_train, y_train, epochs=50)
+
+# Predecir valores
+y_pred = model.predict(X_train)
+
+# Graficar los datos y la línea de regresión
+plt.scatter(X_train, y_train)
+plt.plot(X_train, y_pred, color='red')
+plt.show()
