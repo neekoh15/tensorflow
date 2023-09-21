1cf6931e9a5f2f00bd2a9edb029c794992b54b93
Nicolas Agustin Martinez <59147377+neekoh15@users.noreply.github.com>
2023-09-21 20:45:35 -0300

    Update LrTF.py

--- LrTF.py
+++ LrTF.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 X_train = np.linspace(-1, 1, 100)
 y_train = 2 * X_train + np.random.randn(*X_train.shape) * 0.33
 
-# Crear el modelo
+# Crear
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(1, input_shape=(1,)))
 
